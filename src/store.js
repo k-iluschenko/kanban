13709938dbb845d4683dff9f5112b5a21cf6ed39
k.iluschenko <k.iluschenko@gmail.com>
2019-03-02 16:35:45 +0300

    Сохрание состояния

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
@@ -9,8 +10,8 @@ export default new Vuex.Store({
     newCard: {},
     list: [
     ],
-
   },
+  plugins: [createPersistedState()],
   getters: {
     getList(state) {
       return state.list;
